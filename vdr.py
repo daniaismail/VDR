b308ff48864ab5c3e52937ff544cf5e21768ad22
daniaismail <dani14sssss@gmail.com>
2023-01-30 19:40:37 +0800

    Download VDR from list in txt files

--- vdr.py
+++ vdr.py
@@ -1,83 +1,100 @@
 import imaplib
 import email
+import smtplib
+import sys
 from datetime import datetime, timedelta
 import os
 import fnmatch
+from time import strftime
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.header import decode_header
+import logging
 
-file_path = r'C:\Users\MVMWEB\pythonProject\Email\Downloaded'
-date = (datetime.today()).strftime("%d-%b-%Y")
 today = datetime.today()
-cutoff = today - timedelta(days=2)
+t = today.strftime("%d%m%y")
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%d%m%Y %I:%M:%S%p', filename=f"C:/Users/mvmwe/PycharmProjects/VDR/log/log {t}.txt", level=logging.DEBUG)
+yesterday = datetime.today() - timedelta(1)
+cutoff = today - timedelta(days=1)
 dt = cutoff.strftime('%d-%b-%Y')
+file_path = r'C:\Users\mvmwe\Dropbox\MVMCC\VDR'
+folder_year_path = os.path.join(file_path, str(yesterday.year))
+folder_month_path = os.path.join(folder_year_path, strftime("%b").upper())
 error_file = []
-
-'''
-email_add = "dania@meridiansurveys.com.my"
-password = "e*eM-@FfK$w*"
-
-'''
-email_vdr1 = "vdr1@meridiansurveys.com.my"
-pwd_vdr1 = "x2c(UR*{gfT#"
-
-email_vdr = "vdr@meridiansurveys.com.my"
-pwd_vdr = "T%zf5ccq;ZMc"
-
-email_mvmcc = "mvmcc@meridiansurveys.com.my"
-pwd_mvmcc = "dc)in]}Xzk&%"
-
-email_wgg = "mvmcc@wild-geese-group.com"
-pwd_wgg = "s9nPviD\\"
-
-server_wgg = "mail.wild-geese-group.com"
-server_mssb = "meridian-svr.meridiansurveys.com.my"
-
-fr_vdr1 = ["jmmurni@jmfleet.com.my", "omniemery1@iconoffshore.com.my", "vanessa9@ipsignature3.net", "jmseribesut@jmfleet.com.my", "setiateguh@stationsatcom.commbox.com", "setiazaman@stationsatcom.commbox.com", "express.alpha@jvcmega.com.my", "9mvp9@amosconnect.com", "centusone@ipsignature3.net", "centusthree@ipsignature3.net", "centustwo@ipsignature3.net", "exh@eopl.gtmailplus.com", "iconamara@iconoffshore.com.my", "jmabadi@jmfleet.com.my", "jmpermai@jmfleet.com.my", "skpatriot@skom.com.my", "yinsonperwira@stationsatcommail.com", "skatomik@skom.com.my", "skline79@skom.com.my", "jmehsan@jmfleet.com.my", "ebr@eopl.gtmailplus.com", "Express85@gtmailplus.com", "jmpurnama@jmfleet.com.my", "exn@eopl.gtmailplus.com", "taqwaadam14@gmail.com", "fcbmasindah@outlook.com", "dayang_almira@ipsignature3.net", "ebn@eopl.gtmailplus.com", "vdr.mkbumimas@gmail.com", "pacific.harrier@spoships.com", "master@tourmaline.ss.commbox.com", "lsk.bridge@emas.com"]
-f_name_vdr1 = ["hess\\jm murni", "hess\\omni emery 1", "jxnippon\\vanessa 9", "petrofac\\jm seri besut", "petrofac\\setia teguh", "petrofac\\setia zaman", "pflng\\express alpha", "pflng\\ntp 29", "pttep\\centus one", "pttep\\centus three", "pttep\\centus two", "pttep\\executive honour", "hess\\icon amara", "pttep\\jm abadi", "pttep\\jm permai", "pttep\\sk patriot", "pttep\\yinson perwira", "petrofac\\sk atomik", "pttep\\sk line 79", "petrofac\\jm ehsan", "pttep\\executive brilliance", "petrofac\\express 85", "pttep\\jm purnama", "pttep\\executive excellence", "pttep\\mv taqwa adam", "jxnippon\\mas indah", "jxnippon\\dayang almira", "pttep\\executive benevolence", "pttep\\mas baiduri", "pttep\\pacific harrier", "pttep\\tourmaline", "pttep\\jp88 stork"]
-
-fr_vdr = ["iconamara@iconoffshore.com.my", "dayang_almira@ipsignature3.net", "lsk.bridge@emas.com", "vdr.mkbumimas@gmail.com", "pacific.harrier@spoships.com", "master@tourmaline.ss.commbox.com"]
-f_name_vdr = ["icon amara", "dayang almira", "lewek stork", "mas baiduri", "pacific harrier", "tourmaline"]
-
-fr_mvmcc = ["skprodigy@skom.com.my", "9mvp7@amosconnect.com", "9mvp8@amosconnect.com"]
-f_name_mvmcc = ["pflng\\sk prodigy", "pflng\\ntp 27", "pflng\\ntp 28"]
-
-fr_wgg = ["ntpxxxVII@amosconnect.com"]
-f_name_wgg = ["pflng\\ntp 37"]
 
 if not os.path.exists(file_path):
     os.mkdir(file_path)
 
-def dwl_vdr(email_add, password, server, fr, f_name):
+if not os.path.exists(folder_year_path):
+    os.mkdir(folder_year_path)
+
+if not os.path.exists(folder_month_path):
+    os.mkdir(folder_month_path)
+
+message = "VDR Directory: " + folder_month_path
+# EMAIL INFO
+email_vdr = "vdr@meridiansurveys.com.my"
+pwd_vdr = "T%zf5ccq;ZMc"
+server_mssb = "meridian-svr.meridiansurveys.com.my"
+
+# READ FROM TXT FILE AND APPEND INTO LIST
+vesselemail_list = []
+clientname_list = []
+vesselname_list = []
+
+logging.debug('Logging started..')
+logging.debug('Open email-address.txt')
+try:
+    with open(r'C:\Users\mvmwe\PycharmProjects\VDR\email-address.txt') as f:
+        try:
+            for line in f:
+                vesselemail, vesselname, clientname = line.split(',')
+                vesselemail_list.append(vesselemail)
+                vesselname_list.append(vesselname)
+                clientname_list.append(clientname.replace("\n", ""))
+        except:
+            logging.error('Error info in text file. Check for typo')
+except:
+    logging.exception('Error info in text file or check txt file name')
+    sys.exit()
+
+def dwl_vdr(email_add, password, server, vesselEmail, vesselName):
     imap = imaplib.IMAP4_SSL(server, 993)
     imap.login(email_add, password)
-    imap.select('Inbox')
-    #typ, data = imap.search(None, '(SENTSINCE {0})'.format(date))
+    imap.select('INBOX')
 
     index = 0
 
-    while index < len(fr) and index < len(f_name):
+    while index < len(vesselEmail) and index < len(vesselName):
         try:
-            folder_name = os.path.join(file_path, f_name[index])
+            client_folder = os.path.join(folder_month_path, clientname_list[index])
+
+            if not os.path.exists(client_folder):
+                os.mkdir(client_folder)
+
+            folder_name = os.path.join(client_folder, vesselName[index])
             if not os.path.exists(folder_name):
                 os.mkdir(folder_name)
-
-            typ, data = imap.search(None, '(SINCE %s)' % (dt,), '(FROM %s)' % (fr[index],))
+            logging.debug(f'Download from email {vesselEmail[index]}')
+            typ, data = imap.search(None, '(SINCE %s)' % (dt,), '(FROM %s)' % (vesselEmail[index],))
 
             for num in data[0].split():
                 typ, data = imap.fetch(num, '(RFC822)')
                 raw_email = data[0][1]
-                raw_email_string = raw_email.decode('windows-1252')
+                raw_email_string = raw_email.decode('ISO-8859–1')
                 email_message = email.message_from_string(raw_email_string)
                 subject_name = email_message['subject']
 
-                att_path = "No attachment found from email " + subject_name
+                # att_path = "No attachment found from email " + subject_name
+                logging.debug(f'Email subject: {subject_name}')
                 for part in email_message.walk():
                     try:
                         if part.get_content_maintype() == 'multipart':
                             continue
                         if part.get('Content-Disposition') is None:
                             continue
-                        fileName = part.get_filename()
 
+                        fileName = part.get_filename()
                         if fnmatch.fnmatch(fileName, "*.xls*"):
                             try:
                                 att_path = os.path.join(folder_name, fileName)
@@ -88,7 +105,8 @@ def dwl_vdr(email_add, password, server, fr, f_name):
                                     fp.close()
                             except TypeError as e:
                                 error_file.append(fileName)
-                                pass
+                                continue
+                            logging.debug(f'File downloaded: {fileName}')
 
                         elif fnmatch.fnmatch(fileName, "*.doc*"):
                             try:
@@ -100,24 +118,76 @@ def dwl_vdr(email_add, password, server, fr, f_name):
                                     fp.close()
                             except TypeError as e:
                                 error_file.append(fileName)
-                                pass
+                                continue
+                            logging.debug(f'File downloaded: {fileName}')
 
-                    except TypeError as e:
-                        error_file.append(fileName)
-                        pass
+                        elif fnmatch.fnmatch(fileName, "*.pdf"):
+                            try:
+                                att_path = os.path.join(folder_name, fileName)
+                                print(att_path)
+                                if not os.path.isfile(att_path):
+                                    fp = open(att_path, "wb")
+                                    fp.write(part.get_payload(decode=True))
+                                    fp.close()
+                            except TypeError as e:
+                                error_file.append(fileName)
+                                continue
+                            logging.debug(f'File downloaded: {fileName}')
 
-                print(att_path)
+                        else:
+                            logging.debug(f'No file downloaded from {subject_name}')
+
+                    except (OSError, TypeError) as f:
+                        print(f)
+                        continue
+
+                # print(att_path)
+
+                for response in data:
+                    if isinstance(response, tuple):
+                        data = email.message_from_bytes(response[1])
+                        subject = decode_header(data["Subject"])[0][0]
+                        if isinstance(subject, bytes):
+                            # if it's a bytes type, decode to str
+                            subject = subject.decode()
+                        logging.debug(f'Deleting email {subject}')
+                imap.store(num, "+FLAGS", "\\Deleted")
+
             index += 1
 
-        except TypeError as e:
-            print(e)
-            pass
+        except (OSError, TypeError) as k:
+            print(k)
+            continue
 
-    print(error_file)
+    imap.expunge()
     imap.close()
     imap.logout()
+    logging.debug('Job finished..')
 
-dwl_vdr(email_wgg, pwd_wgg, server_wgg, fr_wgg, f_name_wgg)
-dwl_vdr(email_mvmcc, pwd_mvmcc, server_mssb, fr_mvmcc, f_name_mvmcc)
-dwl_vdr(email_vdr, pwd_vdr, server_mssb, fr_vdr1, f_name_vdr1)
-#dwl_vdr(email_vdr, pwd_vdr, server_mssb, fr_vdr, f_name_vdr)
+def send_email(finishmessage):
+    msg = MIMEMultipart()
+    message = str(finishmessage)
+
+    #message parameters
+    password = "dc)in]}Xzk&%"
+    msg['From'] = "mvmcc@meridiansurveys.com.my"
+    msg['To'] = "mvmcc@meridiansurveys.com.my"
+    msg['Subject'] = "VDR Download"
+
+    msg.attach(MIMEText(message, 'plain'))
+
+    server = smtplib.SMTP('meridian-svr.meridiansurveys.com.my: 587')
+    server.starttls()
+
+    server.login(msg['From'], password)
+
+    server.sendmail(msg['From'], msg['To'], msg.as_string())
+    server.quit()
+
+try:
+    logging.debug('Downloading..')
+    dwl_vdr(email_vdr, pwd_vdr, server_mssb, vesselemail_list, vesselname_list)
+    #send_email(message)
+
+except Exception as e:
+    logging.error("Error occurred", exc_info=True)
